c10dee904773b8eb24ddcf26db29e07bfbb0e17e
benmaia <godlinhomaia@gmail.com>
2022-05-15 23:59:07 +0100

    update path to goinfre in macos

--- vm.sh
+++ vm.sh
@@ -14,7 +14,12 @@ VBoxManage modifyvm $MACHINENAME --nic1 nat
 #Create Disk, connect Debian ISO
 VBoxManage createhd --filename `pwd`/$MACHINENAME/$MACHINENAME_DISK.vdi --size 10000 --format VDI
 VBoxManage storagectl $MACHINENAME --name "SATA Controller" --add sata --controller IntelAhci
-VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  `pwd`/$MACHINENAME/$MACHINENAME_DISK.vdi
+if [$(uname) = Linux]
+then
+		  VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  `pwd`/$MACHINENAME/$MACHINENAME_DISK.vdi
+else
+		  VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  `pwd`/goinfre/$MACHINENAME/$MACHINENAME_DISK.vdi
+fi
 VBoxManage storagectl $MACHINENAME --name "IDE Controller" --add ide --controller PIIX4
 VBoxManage storageattach $MACHINENAME --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "../debian.iso"
 VBoxManage modifyvm $MACHINENAME --boot1 dvd --boot2 disk --boot3 none --boot4 none
