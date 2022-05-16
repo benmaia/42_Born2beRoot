340f6cb0050736cfab34701eab92b4917df6e579
benmaia <godlinhomaia@gmail.com>
2022-05-16 16:03:51 +0100

    sgoinfre

--- vm.sh
+++ vm.sh
@@ -4,7 +4,7 @@
 MACHINENAME=$1
 
 #Create VM
-VBoxManage createvm --name $MACHINENAME --ostype "Debian_64" --register --basefolder `pwd`
+VBoxManage createvm --name $MACHINENAME --ostype "Debian_64" --register --basefolder /System/Volumes/Data/sgoinfre/$(whoami)
 
 #Set memory RAM, virtual memomty and network
 VBoxManage modifyvm $MACHINENAME --ioapic on
@@ -12,19 +12,9 @@ VBoxManage modifyvm $MACHINENAME --memory 4096 --vram 128
 VBoxManage modifyvm $MACHINENAME --nic1 nat
 
 #Create Disk, connect Debian ISO
-if ["$(uname)" == "Linux"]
-then
-		  VBoxManage createhd --filename `pwd`../$MACHINENAME/$MACHINENAME_DISK.vdi --size 10000 --format VDI
-else
-		  VBoxManage createhd --filename `pwd`/sgoinfre/$(whoami)/$MACHINENAME/$MACHINENAME_DISK.vdi --size 10000 --format VDI
-fi
+VBoxManage createhd --filename /System/Volumes/Data/sgoinfre/$(whoami)/$MACHINENAME/$MACHINENAME_DISK.vdi --size 10000 --format VDI
 VBoxManage storagectl $MACHINENAME --name "SATA Controller" --add sata --controller IntelAhci
-if ["$(uname)" == "Linux"]
-then
-		  VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  `pwd`../$MACHINENAME/$MACHINENAME_DISK.vdi
-else
-		  VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  `pwd`/sgoinfre/$(whoami)/$MACHINENAME/$MACHINENAME_DISK.vdi
-fi
+VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  /System/Volumes/Data//sgoinfre/$(whoami)/$MACHINENAME/$MACHINENAME_DISK.vdi
 VBoxManage storagectl $MACHINENAME --name "IDE Controller" --add ide --controller PIIX4
 VBoxManage storageattach $MACHINENAME --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "../debian.iso"
 VBoxManage modifyvm $MACHINENAME --boot1 dvd --boot2 disk --boot3 none --boot4 none
