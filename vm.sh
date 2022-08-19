274b5d5e0dd214b319ff15c5cdad85e3a273a10e
benmaia <godlinhomaia@gmail.com>
2022-08-19 17:23:18 +0100

    documentation

--- vm.sh
+++ vm.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
 
-#The name of your machine
+
+##########################################################
+#                                                        #
+#              The name of your machine                  #
+#                                                        #
+##########################################################
+
 MACHINENAME=$1
 
-#Create VM
+##########################################################
+#                                                        #
+#                     Create VM                          #
+#                                                        #
+##########################################################
+
 VBoxManage createvm --name $MACHINENAME --ostype "Debian_64" --register --basefolder /System/Volumes/Data/sgoinfre/$(whoami)
 
-#Set memory RAM, virtual memomty and network
+##########################################################
+#                                                        #
+#      Set memory RAM, virtual memomty and network       #
+#                                                        #
+##########################################################
+
 VBoxManage modifyvm $MACHINENAME --ioapic on
 VBoxManage modifyvm $MACHINENAME --memory 4096 --vram 128
 VBoxManage modifyvm $MACHINENAME --nic1 nat
 
-#Create Disk, connect Debian ISO
+##########################################################
+#                                                        #
+#      Create Disk Partition, connect Debian ISO         #
+#                                                        #
+##########################################################
+
 VBoxManage createhd --filename /System/Volumes/Data/sgoinfre/$(whoami)/$MACHINENAME/$MACHINENAME_DISK.vdi --size 10000 --format VDI
 VBoxManage storagectl $MACHINENAME --name "SATA Controller" --add sata --controller IntelAhci
 VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  /System/Volumes/Data//sgoinfre/$(whoami)/$MACHINENAME/$MACHINENAME_DISK.vdi
@@ -19,9 +40,20 @@ VBoxManage storagectl $MACHINENAME --name "IDE Controller" --add ide --controlle
 VBoxManage storageattach $MACHINENAME --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "../debian.iso"
 VBoxManage modifyvm $MACHINENAME --boot1 dvd --boot2 disk --boot3 none --boot4 none
 
-#Enable RDP and open port tcp 4242
+##########################################################
+#                                                        #
+#        Enable NAT adapter to ssh connection            #
+#            & port fowarding 4242 access                #
+#                                                        #
+##########################################################
+
 VBoxManage modifyvm $MACHINENAME --vrde on
 VBoxManage modifyvm $MACHINENAME --natpf1 "guestssh,tcp,,4242,,4242"
 
-#Start the VM
+##########################################################
+#                                                        #
+#                    Start the VM                        #
+#                                                        #
+##########################################################
+
 VBoxManage startvm $MACHINENAME --type gui
